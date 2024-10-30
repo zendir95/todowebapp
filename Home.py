b0b3a9ce1f0ce149352d93ae164599037f983d95
ildel <ildelu@gmail.com>
2024-10-30 16:44:51 +0100

    Modified page name, + added About page

--- Home.py
+++ Home.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     new_todo = st.session_state["new_todo"]
     todos.append((new_todo + "\n"))
@@ -10,8 +12,11 @@ def add_todo():
     st.session_state["new_todo"]= ""
 
 st.title("<< My Todo App >>")
-st.subheader("My first web app :)")
-st.write("\"Amazing\"")
+
+st.text_input(label="Enter a Todo", placeholder="Write something to...do",
+              on_change=add_todo, key="new_todo")
+
+st.subheader("Todo list: ", divider="grey")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=f"{index}.{todo}")
@@ -21,8 +26,7 @@ for index, todo in enumerate(todos):
         del st.session_state[f"{index}.{todo}"]
         st.rerun()
 
-st.text_input(label="Enter a Todo", placeholder="Write something to...do",
-              on_change=add_todo, key="new_todo")
+
 
 # st.session_state
 # pip freeze > requirements.txt <<- genera file requirements.txt utile al server per conoscere tutti le library dependencies
---
+++ pages/About.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("<i>\"Amazing\"</i>",
+         unsafe_allow_html=True) # html tags solo in st.write (no title né subheader)
